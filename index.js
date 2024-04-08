ce7ea8ac6bf1747152fba226bd215668d11fffbe
Tomas Casa <tomas.casa123@gmail.com>
2024-04-08 04:49:30 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import loggear from "./src/server.js"
+
+console.log("hello ");
+
+loggear(2,4)
---
+++ src/server.js
@@ -0,0 +1,9 @@
+
+
+
+const loggear = (palabra,palabra2) => {
+    console.log(palabra+palabra2);
+
+}
+
+export default loggear;
